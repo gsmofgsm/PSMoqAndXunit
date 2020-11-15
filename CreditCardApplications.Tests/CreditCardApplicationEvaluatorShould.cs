6c4fc833771893a2efc969c8cfc6ca1d370b2dbe
Qing Ma <gsmofgsm@hotmail.com>
2020-11-15 15:23:57 +0100

    configure mock property to return from a function

--- CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -115,7 +115,7 @@ namespace CreditCardApplications.Tests
             var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
 
             mockValidator.Setup(x => x.IsValid(It.IsAny<string>())).Returns(true);
-            mockValidator.Setup(x => x.LicenseKey).Returns("EXPIRED");
+            mockValidator.Setup(x => x.LicenseKey).Returns(GetLicenseKeyExpiryString);
 
             var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
 
@@ -124,6 +124,12 @@ namespace CreditCardApplications.Tests
             CreditCardApplicationDecision decision = sut.Evaluate(application);
 
             Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decision);
+        }
+
+        string GetLicenseKeyExpiryString()
+        {
+            // E.g. read from vendor-supplied constants file
+            return "EXPIRED";
         }
     }
 }
