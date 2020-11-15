c08785414dfd5b383895406930e3cb0826d9ddf7
Qing Ma <gsmofgsm@hotmail.com>
2020-11-15 16:41:21 +0100

    Verify a method is not called by adding Times.Never

--- CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -180,5 +180,24 @@ namespace CreditCardApplications.Tests
 
             mockValidator.Verify(x => x.IsValid(It.IsAny<string>()), "Frequent flyer numbers should be validated.");
         }
+
+        [Fact]
+        public void NotValidateFrequentFlyerNumberForHighIncomeApplications()
+        {
+            var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+
+            mockValidator.Setup(x => x.ServiceInformation.License.LicenseKey).Returns("OK");
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+
+            var application = new CreditCardApplication
+            {
+                GrossAnnualIncome = 100_000
+            };
+
+            sut.Evaluate(application);
+
+            mockValidator.Verify(x => x.IsValid(It.IsAny<string>()), Times.Never);
+        }
     }
 }
