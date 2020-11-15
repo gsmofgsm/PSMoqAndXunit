8606d0b889c8f4ad95877ada946d7c7cea7ecced
Qing Ma <gsmofgsm@hotmail.com>
2020-11-15 15:46:23 +0100

    ReferWhenLicenseKeyExpired with deeper hierarchy

--- CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -112,10 +112,15 @@ namespace CreditCardApplications.Tests
         [Fact]
         public void ReferWhenLicenseKeyExpired()
         {
+            var mockLicenseData = new Mock<ILicenseData>();
+            mockLicenseData.Setup(x => x.LicenseKey).Returns("EXPIRED");
+            var mockServiceInfo = new Mock<IServiceInformation>();
+            mockServiceInfo.Setup(x => x.License).Returns(mockLicenseData.Object);
             var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+            mockValidator.Setup(x => x.ServiceInformation).Returns(mockServiceInfo.Object);
 
             mockValidator.Setup(x => x.IsValid(It.IsAny<string>())).Returns(true);
-            mockValidator.Setup(x => x.LicenseKey).Returns(GetLicenseKeyExpiryString);
+            //mockValidator.Setup(x => x.LicenseKey).Returns(GetLicenseKeyExpiryString);
 
             var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
 
--- CreditCardApplications/CreditCardApplicationEvaluator.cs
+++ CreditCardApplications/CreditCardApplicationEvaluator.cs
@@ -20,7 +20,7 @@
                 return CreditCardApplicationDecision.AutoAccepted;
             }
 
-            if (_validator.LicenseKey == "EXPIRED")
+            if (_validator.ServiceInformation.License.LicenseKey == "EXPIRED")
             {
                 return CreditCardApplicationDecision.ReferredToHuman;
             }
